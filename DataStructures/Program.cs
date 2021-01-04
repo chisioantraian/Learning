7910f6d9b9d5c27e9bde85ae190dbe6cbef7d8ae
Traian Chiș <chisioantraian@outlook.com>
2021-01-04 23:01:56 +0200

    Binary Search Tree - Insert, traversals

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,31 +1,30 @@
 ﻿using DataStructures.LinkedList;
+using DataStructures.Tree;
 using System;
 
 namespace DataStructures
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            var l = new DoublyLinkedList<int>();
+            var b = new BinarySearchTree();
+            b.Insert(3);
+            b.Insert(8);
+            b.Insert(1);
+            b.Insert(4);
+            b.Insert(6);
+            b.Insert(2);
+            
+            b.Insert(10);
+            b.Insert(9);
+            b.Insert(20);
+            b.Insert(25);
+            b.Insert(15);
+            b.Insert(16);
 
-            l.AddAtPosition(0, 67);
-            l.AddAtPosition(0, 12);
-            l.AddAtPosition(0, 23);
-            l.AddAtPosition(1, 59);
-            l.AddAtPosition(1, 66);
-            l.AddAtPosition(4, 29);
-            l.AddAtPosition(6, 88);
-            l.RemoveLast();
-            l.RemoveAtPosition(2);
-            l.RemoveFirst();
-            l.AddFirst(6);
-            //l.AddAtPosition(8, 16); //error
+            b.InOrderDisplay();
 
-            foreach (var el in l)
-            {
-                Console.WriteLine(el);
-            }
         }
     }
 }
---
+++ DataStructures/Tree/BinarySearchTree.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree
+{
+
+    class BinarySearchTree
+    {
+        private class Node 
+        {
+            internal int Data;
+            internal Node? Left;
+            internal Node? Right;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+        private Node? root;
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+            if (root is null)
+            {
+                root = node;
+                return;
+            }
+
+            Node? current = root;
+            
+            while (current is not null)
+            {
+                Node? parent = current;
+                if (value < current.Data)
+                {
+                    current = current.Left;
+                    if (current is null)
+                        parent.Left = node;
+                }
+                else
+                {
+                    current = current.Right;
+                    if (current is null)
+                        parent.Right = node;
+                }
+            }
+        }
+
+        public void PreOrderDisplay()
+        {
+            PreOrder(root);
+
+            void PreOrder(Node? root)
+            {
+                if (root != null)
+                {
+                    Console.Write(root.Data + " ");
+                    PreOrder(root.Left);
+                    PreOrder(root.Right);
+                }
+            }
+        }
+
+        public void InOrderDisplay()
+        {
+            InOrder(root);
+
+            void InOrder(Node? root)
+            {
+                if (root != null)
+                {
+                    InOrder(root.Left);
+                    Console.Write(root.Data + " ");
+                    InOrder(root.Right);
+                }
+            }
+        }
+
+        public void PostOrderDisplay()
+        {
+            PostOrder(root);
+
+            void PostOrder(Node? root)
+            {
+                if (root != null)
+                {
+                    PostOrder(root.Left);
+                    PostOrder(root.Right);
+                    Console.Write(root.Data + " ");
+                }
+            }
+        }
+
+
+
+        /*public IEnumerable<int> GenerateInorder()
+        {
+            return GenerateInorder(root);
+        }
+
+        private IEnumerable<int> GenerateInorder(Node? node)
+        {
+            if (node != null)
+            {
+                foreach (int i in GenerateInorder(node.Left))
+                    yield return i;
+
+                yield return node.Data;
+
+                foreach (int i in GenerateInorder(node.Right))
+                    yield return i;
+            }
+        }*/
+
+    }
+}
