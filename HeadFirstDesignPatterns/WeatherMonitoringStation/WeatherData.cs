2026f4fb6e10da8b157ee94d3ab449eb582add65
Traian Chiș <chisioantraian@outlook.com>
2021-01-08 22:19:26 +0200

    Weather Monitoring Station - observer interfaces

---
+++ HeadFirstDesignPatterns/WeatherMonitoringStation/WeatherData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPatterns.WeatherMonitoringStation
+{
+    class WeatherData
+    {
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Pressure { get; set; }
+        public void MeasurementsChanged() 
+        {
+            
+        }
+    }
+}
