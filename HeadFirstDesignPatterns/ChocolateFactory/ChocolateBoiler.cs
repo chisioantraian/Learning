237233af4bbe2cbd6e5219c32ae1ac03381320f4
Traian Chiș <chisioantraian@outlook.com>
2021-01-10 00:31:51 +0200

    Chocolate Factory (singleton pattern)

---
+++ HeadFirstDesignPatterns/ChocolateFactory/ChocolateBoiler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPatterns.ChocolateFactory
+{
+    class ChocolateBoiler
+    {
+        private bool isEmpty;
+        private bool isBoiled;
+        private static ChocolateBoiler? instance;
+        private static readonly object padlock = new object();
+
+        private ChocolateBoiler()
+        {
+            isEmpty = true;
+            isBoiled = false;
+        }
+
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    lock (padlock)
+                    {
+                        instance ??= new ChocolateBoiler();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void Fill()
+        {
+            if (isEmpty)
+            {
+                isEmpty = false;
+                isBoiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!isEmpty && isBoiled)
+            {
+                isEmpty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!isEmpty && !isBoiled)
+            {
+                isBoiled = true;
+            }
+        }
+    }
+}
