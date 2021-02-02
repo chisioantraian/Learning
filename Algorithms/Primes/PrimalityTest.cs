867880f4470894b713cdf7ea2a657fb3290f9ef9
Traian Chiș <chisioantraian@outlook.com>
2021-02-02 15:26:05 +0200

    simple primality test

---
+++ Algorithms/Primes/PrimalityTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Primes
+{
+    class PrimalityTest
+    {
+        public static bool IsPrime(int num)
+        {
+            if (num == 2 || num == 3)
+                return true;
+            if (num % 2 == 0 || num % 3 == 0 || num < 5)
+                return false;
+            int sq = (int)Math.Sqrt(num);
+            for (int div = 5; div <= sq; div += 6)
+            {
+                if (num % div == 0 || num % (div + 2) == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static void RunAlg()
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (IsPrime(num))
+                    Console.WriteLine(num);
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            BinarySearch.RunAlg();
+            Primes.PrimalityTest.RunAlg();
         }
     }
 }
