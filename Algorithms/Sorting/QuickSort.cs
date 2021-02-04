49e2ac2f0f1ceed613e2d724e805bd6b757160ce
Traian Chiș <chisioantraian@outlook.com>
2021-02-04 20:39:11 +0200

    began quick  sort

---
+++ Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    class QuickSort
+    {
+        public static void Sort(List<int> list)
+        {
+            Sort(list, 0, list.Count - 1);
+        }
+
+        private static void Sort(List<int> list, int left, int right)
+        {
+
+        }
+    }
+}
