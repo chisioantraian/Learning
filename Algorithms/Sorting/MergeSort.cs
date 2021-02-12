7ec2f459624e9a63cf268ac4708219e08c25eaa8
Traian Chiș <chisioantraian@outlook.com>
2021-02-12 20:28:04 +0200

    MergeSort - partial

---
+++ Algorithms/Sorting/MergeSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    class MergeSort
+    {
+        public static void Sort(List<int> list)
+        {
+            Sort(list, 0, list.Count - 1);
+        }
+        private static void Sort(List<int> list, int left, int right)
+        {
+            if (left < right)
+            {
+                int m = (left + right) / 2;
+                Sort(list, left, m);
+                Sort(list, m + 1, right);
+                Merge(list, left, m, right);
+            }
+        }
+
+        private static void Merge(List<int> list, int left, int m, int right)
+        {
+            //todo
+        }
+    }
+}
---
+++ CSharpNotes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
