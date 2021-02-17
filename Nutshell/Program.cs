27f9e1b89e1eb9f2f2f9a75f0eadbffd28eba7f3
Traian Chiș <chisioantraian@outlook.com>
2021-02-17 22:34:51 +0200

    factorial example

--- Nutshell/Program.cs
+++ Nutshell/Program.cs
@@ -1,16 +1,29 @@
 ﻿using Nutshell.Others;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Nutshell
 {
     class Program
     {
+        static int Factorial(int x)
+        {
+            int Fact(int x, int accumulator) => x switch
+            {
+                <= 1 => accumulator,
+                _ => Fact(x - 1, x * accumulator)
+            };
+            
+            return Fact(x, 1);
+        }
         static void Main(string[] args)
         {
-            VarianceExample.Run();
+            WriteLine($"Factorial of 2 : {Factorial(2)}");
+            WriteLine($"Factorial of 7 : {Factorial(7)}");
         }
     }
 }
