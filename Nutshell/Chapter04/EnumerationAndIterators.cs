a00e456227ca1010ac7c3dd15ba5ed189ba37d5e
Traian Chiș <chisioantraian@outlook.com>
2021-02-23 19:01:42 +0200

    enumeration examples

---
+++ Nutshell/Chapter04/EnumerationAndIterators.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutshell.Chapter04
+{
+    class EnumerationAndIterators
+    {
+        public static void Run()
+        {
+            //IterateThroughCharacters();
+            //CollectionInitializersExample();
+            IteratorExample();
+        }
+
+        private static void IterateThroughCharacters()
+        {
+            using (var enumerator = "beer".GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    var element = enumerator.Current;
+                    Console.WriteLine(element);
+                }
+            }
+        }
+
+        private static void CollectionInitializersExample()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+            //...
+            List<int> l = new List<int>();
+            l.Add(1);
+            l.Add(2);
+            l.Add(3);
+        }
+
+        private static void IteratorExample()
+        {
+            foreach (int fib in Fibs(12))
+            {
+                Console.Write(fib + " ");
+            }
+        }
+
+        private static IEnumerable<int> Fibs(int fibCount)
+        {
+            for (int i = 0, prevFib = 1, curFib = 1; i < fibCount; i++)
+            {
+                yield return prevFib;
+                int newFib = prevFib + curFib;
+                prevFib = curFib;
+                curFib = newFib;
+            }
+        }
+    }
+}
--- Nutshell/Program.cs
+++ Nutshell/Program.cs
@@ -10,20 +10,10 @@ namespace Nutshell
 {
     class Program
     {
-        static int Factorial(int x)
-        {
-            int Fact(int x, int accumulator) => x switch
-            {
-                <= 1 => accumulator,
-                _ => Fact(x - 1, x * accumulator)
-            };
-            
-            return Fact(x, 1);
-        }
+
         static void Main(string[] args)
         {
-            WriteLine($"Factorial of 2 : {Factorial(2)}");
-            WriteLine($"Factorial of 7 : {Factorial(7)}");
+            Chapter04.EnumerationAndIterators.Run();
         }
     }
 }
